9e611c34b850c58516982d4e76049e7859e19bfe
chenhewei <1990945244@qq.com>
2023-02-07 16:17:44 +0800

    first commit

---
+++ src/main/java/com/wei/pojo/Demo01.java
@@ -0,0 +1,15 @@
+package com.wei.pojo;
+
+/**
+ * @Author ChenHeWei
+ * @Date 2023/2/7 16:15
+ * @PackageName:com.wei.pojo
+ * @ClassName: Demo01
+ * @Description: TODO
+ * @Version 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+
+    }
+}
