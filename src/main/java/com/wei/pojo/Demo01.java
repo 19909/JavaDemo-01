27b606d7d9f90405a79ac82bb81ee40554842a39
chenhewei <1990945244@qq.com>
2023-02-07 16:28:56 +0800

    System commit

--- src/main/java/com/wei/pojo/Demo01.java
+++ src/main/java/com/wei/pojo/Demo01.java
@@ -11,5 +11,11 @@ package com.wei.pojo;
 public class Demo01 {
     public static void main(String[] args) {
 
+        System.out.println("你好,Java");
+        System.out.println(System.getProperty("java.version"));//1.8.0_121      --Java版本
+        System.out.println(System.getProperty("java.home"));//D:\JavaEE\JDK\jre --java的运行环境路径
+        System.out.println(System.getProperty("os.name"));//Windows 10          --操作系统的名字
+        System.out.println(System.getProperty("user.home"));//C:\Users\19909    --用户的家目录
+
     }
 }
