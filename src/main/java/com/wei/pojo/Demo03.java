a7d4348812c79d185874f952f51672241b528316
chenhewei <1990945244@qq.com>
2023-02-07 16:51:29 +0800

    基本数据类型转换

---
+++ src/main/java/com/wei/pojo/Demo03.java
@@ -0,0 +1,26 @@
+package com.wei.pojo;
+
+/**
+ * @Author ChenHeWei
+ * @Date 2023/2/7 16:42
+ * @PackageName:com.wei.pojo
+ * @ClassName: Demo03
+ * @Description: TODO
+ * @Version 1.0
+ * 强制类型装换
+ */
+public class Demo03 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 20.0;
+
+        //小范围向大范围转换 （自动转换）
+        double s = (double) a;
+
+        //大范围向小范围转换 （手动转换）
+        int t = (int) b;
+//        System.out.println(s);
+        System.out.println(t);
+    }
+}
